8e4cb303f7ed2ad20fe2d7245f9091a1c3f5ba2a
Skylar <srs5sb@virginia.edu>
2017-03-15 07:28:27 -0400

    This sort of works?

---
+++ TheNameGame/ChoiceView.swift
@@ -0,0 +1,25 @@
+//
+//  ChoiceView.swift
+//  TheNameGame
+//
+//  Created by Skylar Spears on 3/14/17.
+//  Copyright © 2017 Skylar Spears. All rights reserved.
+//
+
+import UIKit
+
+class ChoiceView: UIView {
+    //If this goes out please check the file owner of the view
+
+   
+    /*required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        UINib(nibName: "Choice", bundle: nil).instantiate(withOwner: self, options: nil)
+        
+        //UINib(nibName: "Choice", bundle: nil).instantiate(withOwner: self, options: nil)
+        addSubview(View)
+        View.frame = self.bounds
+    }*/
+
+
+}
---
+++ TheNameGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheNameGame
+//
+//  Created by Skylar Spears on 3/14/17.
+//  Copyright © 2017 Skylar Spears. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
